2b8a3380d635bb500bece20784eff8dd8756f52e
rubythebelle <rubybellekim@gmail.com>
2023-10-31 14:33:30 -0700

    basic fomula done

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,51 @@ function App() {
     const newDice = [];
 
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: true,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   }
 
   const diceElement = dice.map((die) => (
-    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
   ));
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instruction">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="die-container">{diceElement}</div>
       <button className="roll-btn" onClick={rollDice}>
         Roll
--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,8 +1,20 @@
 import "../App.css";
 
 function Die(props) {
+  //direct styling way when isHeld is true ->
+  //   const styles = {
+  //     backgroundColor: props.isHeld ? "#59E391" : "white"
+  // }
   return (
-    <div className={props.isHeld == true ? "die-held" : "die-items"}>
+    <div
+      className={props.isHeld ? "die-held" : "die-items"}
+      onClick={props.holdDice}
+    >
+      {/* <div 
+      className="die-items" 
+      style={styles}
+    > */}
+
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
